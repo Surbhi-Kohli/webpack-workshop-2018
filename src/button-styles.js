56acadd3637eb2dc82d50f9cb4aad7780f294f6d
Surbhi-Kohli <surbhi.kohli@walmart.com>
2024-01-05 13:32:53 +0530

    button-style changes

--- src/button-styles.js
+++ src/button-styles.js
@@ -1,5 +1,9 @@
-const red = "color: red;";
-const blue = "color: blue;";
-const makeColorStyle = color => `color: ${color};`;
+//commmon.js named exports
+const red=  "color:red;"
+const blue = "color:blue;"
+const makeColorStyle = (color)=>`color:${color}`
+// exports.red = red; with named exports, tree shaking would not happen
+// exports.blue = blue;
+// exports.makeColorStyle = makeColorStyle;
 
-export {red, blue, makeColorStyle}
+export {red,blue,makeColorStyle}
