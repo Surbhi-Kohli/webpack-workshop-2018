34fe873626aefcf5b1e34a1894f13d5fff5ce868
Surbhi-Kohli <surbhi.kohli@walmart.com>
2024-02-17 22:18:55 +0530

    hmr

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,5 +1,11 @@
 module.exports = () => ({
   output: {
     filename: "[chunkhash].js"
+  },module:{
+    rules:[
+      { test:/\.css$/,
+      use:["style-loader","css-loader"]
+    }
+  ]
   }
 });
